e89f4efe7d206b9e3be5a361a1c4aca060296a2e
Matilda Mared <matildamared@live.se>
2022-01-20 14:35:49 +0100

    add basic backend

---
+++ client/data/meetups.js
@@ -0,0 +1,45 @@
+const meetups = [
+  {
+    id: 1,
+    ownerId: 1736,
+    title: "Kodkväll för nybörjare",
+    category: "programming",
+    description:
+      "Cat ipsum dolor sit amet, it's 3am, time to create some chaos bathe private parts with tongue then lick owner's face hell is other people so cat sit like bread. Lay on arms while you're using the keyboard put butt in owner's face or make it to the carpet before i vomit mmmmmm and claw drapes, and refuse to drink water except out of someone's glass. Why dog in house? i'm the sole ruler of this home and its inhabitants smelly, stupid dogs, inferior furballs time for night-hunt, human freakout mice blow up sofa in 3 seconds why can't i catch that stupid red dot. Kitty time. Steal mom's crouton while she is in the bathroom instead of drinking water from the cat bowl, make sure to steal water from the toilet and intrigued by the shower, but give me attention or face the wrath of my claws scratch me now! stop scratching me! scratch my tummy actually i hate you now fight me yet the cat was chasing the mouse. Kitty loves pigs eat owner's food, get video posted to internet for chasing red dot so jump on human and sleep on her all night long be long in the bed, purr in the morning and then give a bite to every human around for not waking up request food, purr loud scratch the walls, the floor, the windows, the humans and mark territory, but intently sniff hand. Cats are a queer kind of folk hate dogs, for flee in terror at cucumber discovered on floor, yet kitty poochy i vomit in the bed in the middle of the night.",
+    date: new Date("2022-02-18T18:00:00"),
+    location: "Mr Cake, Lilla Badhusgatan 2b, Göteborg",
+    imgUrl:
+      "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
+    attendees: [
+      { name: "Matilda", id: 1736 },
+      { name: "Sofia", id: 1319 },
+      { name: "Sara", id: 7382 },
+    ],
+    comments: [
+      {
+        name: "Matilda",
+        comment: "This is a great meetup!",
+      },
+    ],
+  },
+  {
+    id: 2,
+    ownerId: 1736,
+    title:
+      "Pokémon meetup (vi livesänder speedrun av pokémon Emerald på Twitch!)",
+    category: "gaming",
+    description:
+      "Cat ipsum dolor sit amet, find a way to fit in tiny box for fooled again thinking the dog likes me, mice. Twitch tail in permanent irritation groom forever, stretch tongue and leave it slightly out, blep touch water with paw then recoil in horror lounge in doorway, for find box a little too small and curl up with fur hanging out . Ears back wide eyed kitty power i like to spend my days sleeping and eating fishes that my human fished for me we live on a luxurious yacht, sailing proudly under the sun, i like to walk on the deck, watching the horizon, dreaming of a good bowl of milk and eat my own ears good morning sunshine. Fight an alligator and win roll on the floor purring your whiskers off yet meow to be let out yet hiding behind the couch until lured out by a feathery toy lay on arms while you're using the keyboard. Pet my belly, you know you want to; seize the hand and shred it! oooo! dangly balls! jump swat swing flies so sweetly to the floor crash move on wash belly nap but nya nya nyan. X chase imaginary bugs, but cats go for world domination or experiences short bursts of poo-phoria after going to the loo. Howl on top of tall thing steal the warm chair right after you get up, curl up and sleep on the freshly laundered towels meow for food, then when human fills food dish, take a few bites of food and continue meowing. If it fits, i sits. I hate cucumber pls dont throw it at me. Meow cat walks in keyboard .",
+    date: new Date("2022-03-12T19:00:00"),
+    location: "Kaptensgatan 10E, Halmstad",
+    imgUrl:
+      "https://images.unsplash.com/photo-1627856013091-fed6e4e30025?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+    attendees: [
+      { name: "Matilda", id: 1736 },
+      { name: "Sara", id: 7382 },
+    ],
+    comments: [],
+  },
+];
+
+export default meetups;
---
+++ client/data/users.js
@@ -0,0 +1,28 @@
+const users = [
+  {
+    id: 1736,
+    username: "atara",
+    password: "test1234",
+    firstName: "Matilda",
+    attending: [1, 2],
+    createdMeetups: [1, 2],
+  },
+  {
+    id: 1319,
+    username: "zepsi",
+    password: "test1234",
+    firstName: "Sofia",
+    attending: [1, 2],
+    createdMeetups: [],
+  },
+  {
+    id: 7382,
+    username: "blomman",
+    password: "test1234",
+    firstName: "Sara",
+    attending: [1],
+    createdMeetups: [],
+  },
+];
+
+export default users;
