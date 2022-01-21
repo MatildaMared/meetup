5bd7b3c019f16912de361f037c60fa6554773fd0
sarablom <sara.cecilia.blom@gmail.com>
2022-01-21 16:34:34 +0100

    add links for navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,30 +1,48 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components/macro";
-
-// interface Props {
-//   items: string[];
-// }
 
 function Navbar() {
   const [isSelected, setIsSelected] = useState<string>("Home");
-  const items= ["Home", "Find Meetups", "Sign in", "Sign up"];
+  const items = ["Home", "Find Meetups", "Sign in", "Sign up"];
 
   function addClassHandler(e: any) {
     setIsSelected(e.target.innerHTML);
+    console.log(isSelected);
+    
   }
 
   return (
     <NavbarWrapper>
       <ListWrapper>
-        {items.map((item) => (
-          <ListElement
-            key={item}
-            onClick={(e: object) => addClassHandler(e)}
-            className={isSelected === item ? "selected" : ""}
-          >
-            {item}
-          </ListElement>
-        ))}
+        <Link
+          to="/"
+          onClick={(e: object) => addClassHandler(e)}
+          className={isSelected === items[0] ? "selected" : ""}
+        >
+          {items[0]}
+        </Link>
+        <Link
+          to="/meetups"
+          onClick={(e: object) => addClassHandler(e)}
+          className={isSelected === items[1] ? "selected" : ""}
+        >
+          {items[1]}
+        </Link>
+        <Link
+          to="/signin"
+          onClick={(e: object) => addClassHandler(e)}
+          className={isSelected === items[2] ? "selected" : ""}
+        >
+          {items[2]}
+        </Link>
+        <Link
+          to="/signup"
+          onClick={(e: object) => addClassHandler(e)}
+          className={isSelected === items[3] ? "selected" : ""}
+        >
+          {items[3]}
+        </Link>
       </ListWrapper>
     </NavbarWrapper>
   );
