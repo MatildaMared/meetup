3182326804cf187b434a09abd686978aa6e41632
Sofia Hervéus <sofia@herveus.se>
2022-01-21 17:34:12 +0100

    add some info to the event

--- client/src/components/meetupcard/MeetupCard.tsx
+++ client/src/components/meetupcard/MeetupCard.tsx
@@ -19,10 +19,15 @@ function MeetupCard() {
   return (
     <div>
       {context.singleMeetup && (
-        <div>
-          <img src={context.singleMeetup.imgUrl} alt="" />
-          <h3>{context.singleMeetup.title}</h3>
-        </div>
+        <StyledCard>
+          <Image src={context.singleMeetup.imgUrl} alt="" />
+          <Info>
+            <h3>{context.singleMeetup.title}</h3>
+            <small>{context.singleMeetup.date}</small>
+            <h4>Event Info</h4>
+            <p>{context.singleMeetup.description}</p>
+          </Info>
+        </StyledCard>
       )}
     </div>
   );
@@ -30,6 +35,52 @@ function MeetupCard() {
 
 export default MeetupCard;
 
-export const StyledCard = styled.div`
-  background-color: lightblue;
+const StyledCard = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap");
+
+  font-family: "Roboto", sans-serif;
+  display: flex;
+  max-width: 60vw;
+  padding: 3rem;
+  margin: 10px auto;
+  flex-direction: column;
+  background-color: #daf4fd;
+  border-radius: 5px;
+
+  h3 {
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+
+  small {
+    color: darkgrey;
+  }
+
+  h4 {
+    margin-top: 1.6rem;
+  }
+
+  @media (max-width: 640px) {
+    max-width: 400px;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 50vw;
+  margin: 0 auto;
+
+  @media (max-width: 640px) {
+    max-width: 300px;
+  }
+`;
+
+const Info = styled.div`
+  max-width: 50vw;
+  margin: 0 auto;
+  padding: 1rem;
+  background-color: #ffffff;
+
+  @media (max-width: 640px) {
+    max-width: 300px;
+  }
 `;
