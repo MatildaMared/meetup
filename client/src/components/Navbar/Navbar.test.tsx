a8bc3141dd00cd4caf5872a234b72022dcd0f12c
sarablom <sara.cecilia.blom@gmail.com>
2022-01-23 08:54:05 +0100

    make tests for navbar succced

--- client/src/components/Navbar/Navbar.test.tsx
+++ client/src/components/Navbar/Navbar.test.tsx
@@ -1,36 +1,46 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { MemoryRouter as Router } from "react-router-dom";
 import Navbar from "./Navbar";
 
 describe("navbar component", () => {
   it("renders without crashing", () => {
-    // render(<Navbar />);
+    render(
+      <Router>
+        <Navbar />
+      </Router>
+    );
   });
 
-//   it("shows links Home, Find Meetups, Sign in, Sign up when user is not logged in", () => {
-//     render(<Navbar />);
+  it("shows links Home, Find Meetups, Sign in, Sign up when user is not logged in", () => {
+    render(
+      <Router>
+        <Navbar />
+      </Router>
+    );
+    const listitems = screen.getAllByRole("listitem");
+
+    expect(listitems.length).toBe(4);
+    expect(listitems[0]).toHaveTextContent("Home");
+    expect(listitems[1]).toHaveTextContent("Find Meetups");
+    expect(listitems[2]).toHaveTextContent("Sign in");
+    expect(listitems[3]).toHaveTextContent("Sign up");
+  });
+});
+
+//Run this tests when able to sign in
+//user can only see login and sign up page when not logged in
+//user can only see My page and Sign out page when logged in
+
+//   it("shows links Home, Find Meetups, My Page, Sign out when user is logged in", () => {
+//     render(    <Router>
+        // <Navbar />
+        // </Router>);
 //     const listitems = screen.getAllByRole("listitem");
 
 //     expect(listitems.length).toBe(4);
 //     expect(listitems[0]).toHaveTextContent("Home");
 //     expect(listitems[1]).toHaveTextContent("Find Meetups");
-//     expect(listitems[2]).toHaveTextContent("Sign in");
-//     expect(listitems[3]).toHaveTextContent("Sign up");
+//     expect(listitems[2]).toHaveTextContent("My Page");
+//     expect(listitems[3]).toHaveTextContent("Sign out");
 //   });
-});
-
-  //Run this tests when able to sign in
-  //user can only see login and sign up page when not logged in
-  //user can only see My page and Sign out page when logged in
-
-  //   it("shows links Home, Find Meetups, My Page, Sign out when user is logged in", () => {
-  //     render(<Navbar items={items} />);
-  //     const listitems = screen.getAllByRole("listitem");
-
-  //     expect(listitems.length).toBe(4);
-  //     expect(listitems[0]).toHaveTextContent("Home");
-  //     expect(listitems[1]).toHaveTextContent("Find Meetups");
-  //     expect(listitems[2]).toHaveTextContent("My Page");
-  //     expect(listitems[3]).toHaveTextContent("Sign out");
-  //   });
-
