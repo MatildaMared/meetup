68688143a7f38d67b29ee0c9b4f8bf04ed48c407
sarablom <sara.cecilia.blom@gmail.com>
2022-01-23 11:15:07 +0100

    add context in test upcomingmeetups

--- client/src/components/upcomingmeetups/UpcomingMeetups.test.tsx
+++ client/src/components/upcomingmeetups/UpcomingMeetups.test.tsx
@@ -1,25 +1,49 @@
+import { useContext } from "react";
+import { MeetupContext } from "../../context/MeetupContext";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { ScriptElementKind } from "typescript";
+import { MeetupProvider } from "../../context/MeetupContext";
 import UpcomingMeetups from "./UpcomingMeetups";
 
 describe("Upcoming meetups component", () => {
+  // const allMeetups: [{}] = [
+  //   { id: "61ea80effe705abdc1db26e8",
+  //     title: "Pokémon meetup (vi livesänder speedrun av pokémon Emerald på Twitch!)", 
+  //   ownerId: "61ea80bcfe705abdc1db26e4",
+  //   imgUrl: "https://images.unsplash.com/photo-1627856013091-fed6e4e30025…G90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+  //   location: "Online",
+  //   date: "2022-03-10T17:00:00.000Z"
+  //  }
+  // ]
   it("renders without crashing", () => {
-    render(<UpcomingMeetups />);
+    render(
+      <MeetupProvider>
+        <UpcomingMeetups />
+      </MeetupProvider>
+    );
   });
 
   it("displays heading with text 'Upcoming Meetups'", () => {
-    render(<UpcomingMeetups />);
+    render(
+      <MeetupProvider>
+        <UpcomingMeetups />
+      </MeetupProvider>
+    );
     const header = screen.getByRole("heading");
-    const text = "Upcoming Meetups"
-    expect(header).toHaveTextContent(text);   
+    const text = "Upcoming Meetups";
+    expect(header).toHaveTextContent(text);
   });
 
-  it("displays 5 number of meetupCards", () => {
-
-  })
+  it("displays 1 number of meetupCards", () => {
+    render(
+      <MeetupProvider>
+        <UpcomingMeetups />
+      </MeetupProvider>
+    );
+    const listitems = screen.getAllByRole("listitem");
+    expect(listitems.length).toBe(1);
+  });
 });
-
 
 //displays title of meetup in card
 //displays name of organizer of meetup in card
