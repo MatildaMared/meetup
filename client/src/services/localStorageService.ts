b9d22e1ea788f96b422ff413ad12c6a1739458f4
Matilda Mared <matildamared@live.se>
2022-01-24 07:24:01 +0100

    add localStorage service functions

---
+++ client/src/services/localStorageService.ts
@@ -0,0 +1,17 @@
+export function saveTokenInLocalStorage(token: string): void {
+  localStorage.setItem("token", token);
+}
+
+export function saveUserInLocalStorage(user: any): void {
+  localStorage.setItem("user", user);
+}
+
+export function getUserFromLocalStorage(): string | null {
+  const user = localStorage.getItem("user");
+  return user ? JSON.parse(user) : null;
+}
+
+export function getTokenFromLocalStorage(): string | null {
+  const token = localStorage.getItem("token");
+  return token ? token : null;
+}
