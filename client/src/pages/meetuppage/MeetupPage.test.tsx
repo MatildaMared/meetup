bd1e0aa638d78a8f6f5087821eac56538f7e3a15
Sofia Hervéus <sofia@herveus.se>
2022-01-24 19:44:39 +0100

    Start working on tests for MeetupPage

--- client/src/pages/meetuppage/MeetupPage.test.tsx
+++ client/src/pages/meetuppage/MeetupPage.test.tsx
@@ -1,8 +1,18 @@
 import { render } from "@testing-library/react";
 import MeetupPage from "./MeetupPage";
 
-describe("Testing for the EventPage", () => {
+describe("Testing for the MeetupPage", () => {
   it("render the MeetupPage component without crashing", () => {
     render(<MeetupPage />);
+  });
+
+  // it("checks that the MeetupCard-component is renderd in the MeetupPage", () => {
+  //   const { getByText } = render(<MeetupPage />);
+  //   expect(getByText(/submit/i)).toBeInTheDocument();
+  // });
+
+  it("checks that the Comment-component is renderd in the MeetupPage", () => {
+    const { getByText } = render(<MeetupPage />);
+    expect(getByText(/submit/i)).toBeInTheDocument();
   });
 });
--- client/src/pages/meetuppage/MeetupPage.tsx
+++ client/src/pages/meetuppage/MeetupPage.tsx
@@ -4,13 +4,22 @@ import { getSingleMeetup } from "../../services/meetupService";
 import { getUserById } from "../../services/userService";
 import { Meetup } from "../../models/Meetup";
 import { User } from "../../models/User";
+import {
+  getTokenFromLocalStorage,
+  getUserFromLocalStorage,
+} from "../../services/localStorageService";
 import MeetupCard from "../../components/meetupcard/MeetupCard";
 import Comment from "../../components/comment/Comment";
+import styled from "styled-components";
 
 function MeetupPage() {
   const [singleMeetup, setSingleMeetup] = useState<Meetup | null>(null);
   const [user, setUser] = useState<User | null>(null);
+  const [attending, setAttending] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const { meetupid } = useParams();
+  const token = getTokenFromLocalStorage();
+  const thisUser = getUserFromLocalStorage();
 
   async function getMeetup(id: string) {
     const meetupInfo = await getSingleMeetup(id);
@@ -32,12 +41,63 @@ function MeetupPage() {
     }
   }, [singleMeetup]);
 
+  const amAttending = (): void => {
+    singleMeetup?.attendees.map((attendees) => {
+      if (attendees.id === thisUser?.id) {
+        console.log(attendees.id, thisUser?.id);
+        console.log("hello");
+        setAttending(true);
+      } else if (attendees.id !== thisUser?.id) {
+        console.log("in else", attendees.id, thisUser?.id);
+        setAttending(false);
+      }
+    });
+  };
+  console.log(thisUser);
+
+  useEffect(() => {
+    amAttending();
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
   return (
-    <>
+    <StyledPage>
+      {isLoggedIn && (
+        <Button>{attending ? "Leave meetup" : "Attend Meetup"}</Button>
+      )}
       <MeetupCard meetup={singleMeetup as Meetup} user={user as User} />
       <Comment />
-    </>
+    </StyledPage>
   );
 }
 
 export default MeetupPage;
+
+const StyledPage = styled.div`
+  position: relative;
+`;
+
+const Button = styled.button`
+  padding: 0.4rem 0.9rem;
+  display: block;
+  position: absolute;
+  right: 22.4rem;
+  top: 3.5rem;
+  background-color: #fff;
+  border: 2px solid lightblue;
+  border-radius: 4px;
+  color: lightblue;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.5);
+
+  &:hover {
+    background-color: #eee;
+  }
+
+  &:active {
+    transform: scale(0.98);
+  }
+`;
