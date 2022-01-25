a0c38e1b854af5758445a868ee7e8b57994e0fae
Matilda Mared <matildamared@live.se>
2022-01-25 14:16:58 +0100

    remove unnecessary test

--- client/src/pages/createPage/CreatePage.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import CreatePage from "./CreatePage";
-
-describe("Create Page", () => {
-  it("renders without crashing", () => {
-    render(<CreatePage />);
-  });
-});
