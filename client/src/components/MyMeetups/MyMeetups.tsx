a9aba7104e632dc17ade3f570084f8adf01aec1a
Matilda Mared <matildamared@live.se>
2022-01-27 10:50:50 +0100

    add comment

--- client/src/components/MyMeetups/MyMeetups.tsx
+++ client/src/components/MyMeetups/MyMeetups.tsx
@@ -17,6 +17,7 @@ function MyMeetups(props: { meetups: [] | [Meetup] }) {
         return meetup.ownerId === userId;
       });
 
+      // update filteredMeetups
       setFilteredMeetups(filtered as [] | [Meetup]);
     }
   }, [meetups]);
